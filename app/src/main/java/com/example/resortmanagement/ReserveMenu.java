279f916db1c31288a625621e956adf9121c1d94a
Mia <34622179+mialotus@users.noreply.github.com>
2019-11-16 22:23:45 -0500

    add amenity.xml

---
+++ app/src/main/java/com/example/resortmanagement/ReserveMenu.java
@@ -0,0 +1,14 @@
+package com.example.resortmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReserveMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amenity);
+    }
+}
